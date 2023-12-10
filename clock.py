f81c45f8f250daa922f8a59dae9fb1a9418644b1
Samuele Vianello <samuele.vianello2002@gmail.com>
2023-12-10 22:11:09 +0100

    edited dateformat, added base for timezones

--- clock.py
+++ clock.py
@@ -1,33 +1,35 @@
 import streamlit as st
 import time
-days = ["Mon", "Tue", "Wed", "Thu", "Fry", "Sat", "Sun"]
-#link_gif = "https://www.icegif.com/wp-content/uploads/snow-icegif-29.gif" 
-#link_gif = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a2205e2c-52eb-46b3-89fb-bd0fd5da780e/dbyn08r-83e1c070-bfa6-48da-ae25-b7d7409639fe.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2EyMjA1ZTJjLTUyZWItNDZiMy04OWZiLWJkMGZkNWRhNzgwZVwvZGJ5bjA4ci04M2UxYzA3MC1iZmE2LTQ4ZGEtYWUyNS1iN2Q3NDA5NjM5ZmUuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.pFhMtU06SUSgNo6CA5fLFNJlv9h69s9oE89E2E6iAGk"
-#color = "white"
-
-link_gif = "https://f8n-production.s3.us-east-2.amazonaws.com/collections/awzn1l17u-beach%20relax-export.gif"
-color = "black"
+from zoneinfo import ZoneInfo, available_timezones
+from datetime import datetime
 
 st.set_page_config(
   page_title="Clock",
   page_icon="🕔"
 )
 
+#link_gif = "https://www.icegif.com/wp-content/uploads/snow-icegif-29.gif" 
+#link_gif = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a2205e2c-52eb-46b3-89fb-bd0fd5da780e/dbyn08r-83e1c070-bfa6-48da-ae25-b7d7409639fe.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2EyMjA1ZTJjLTUyZWItNDZiMy04OWZiLWJkMGZkNWRhNzgwZVwvZGJ5bjA4ci04M2UxYzA3MC1iZmE2LTQ4ZGEtYWUyNS1iN2Q3NDA5NjM5ZmUuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.pFhMtU06SUSgNo6CA5fLFNJlv9h69s9oE89E2E6iAGk"
+color = "white"
 
-time_now = time.localtime()
-time_to_print = ""
-if time_now.tm_hour <10:
-    time_to_print += f"0{time_now.tm_hour}:"
-else:
-    time_to_print += f"{time_now.tm_hour}:"
-if time_now.tm_min <10:
-    time_to_print += f"0{time_now.tm_min}:"
-else:
-    time_to_print += f"{time_now.tm_min}:"
-if time_now.tm_sec <10:
-    time_to_print += f"0{time_now.tm_sec}"
-else:
-    time_to_print += f"{time_now.tm_sec}"
+link_gif = "https://f8n-production.s3.us-east-2.amazonaws.com/collections/awzn1l17u-beach%20relax-export.gif"
+#color = "black"
+
+
+if "timezones" not in st.session_state:
+    timezones = available_timezones()
+    filtered_time_zones = [tz for tz in timezones if "/" in tz and "Etc" not in tz]
+    st.session_state.timezones = filtered_time_zones.sort()
+
+if "timezone" not in st.session_state:
+    st.session_state.timezone = "Europe/Rome"    
+
+zoneinfo = ZoneInfo(st.session_state.timezone)
+dt = datetime.now(zoneinfo)
+  
+
+time_to_print = dt.strftime("%H:%M:%S")
+date_to_print = dt.strftime("%a %d/%m/%Y")
     
 st.title(f"")
 
@@ -40,7 +42,7 @@ hide_st_style = """
 """
 st_title = f"""
 <div style="position: absolute; top: 150px; left: -200px; height: 100vh; font-size: 35px; font-weight: bold; color: {color}">
-    {days[time_now.tm_wday]} {time_now.tm_mday}/{time_now.tm_mon}/{time_now.tm_year}
+    {date_to_print}
 </div>
 """
 st.markdown(st_title, unsafe_allow_html=True)
