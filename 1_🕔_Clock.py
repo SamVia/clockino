ea92877c7eb84dd33efea0757a08b7368d9a3199
Samuele Vianello <samuele.vianello2002@gmail.com>
2023-12-11 17:22:01 +0100

    moved messages added color choice

--- 1_🕔_Clock.py
+++ 1_🕔_Clock.py
@@ -14,19 +14,24 @@ st.set_page_config(
 
 #link_gif = "https://www.icegif.com/wp-content/uploads/snow-icegif-29.gif" 
 link_gif = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a2205e2c-52eb-46b3-89fb-bd0fd5da780e/dbyn08r-83e1c070-bfa6-48da-ae25-b7d7409639fe.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2EyMjA1ZTJjLTUyZWItNDZiMy04OWZiLWJkMGZkNWRhNzgwZVwvZGJ5bjA4ci04M2UxYzA3MC1iZmE2LTQ4ZGEtYWUyNS1iN2Q3NDA5NjM5ZmUuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.pFhMtU06SUSgNo6CA5fLFNJlv9h69s9oE89E2E6iAGk"
-color = "white"
+
 
 #link_gif = "https://f8n-production.s3.us-east-2.amazonaws.com/collections/awzn1l17u-beach%20relax-export.gif"
-#color = "black"
+
 
 
 
 if "timezone" not in st.session_state:
     st.session_state.timezone = "Europe/Rome"    
 
+if "color" not in st.session_state:
+    st.session_state.color = "#ffffff"
+
+if "screen_dim" not in st.session_state:
+    st.session_state.screen_dim = [1920, 1080]
+
 zoneinfo = ZoneInfo(st.session_state.timezone)
 dt = datetime.now(zoneinfo)
-  
 
 time_to_print = dt.strftime("%H:%M:%S")
 date_to_print = dt.strftime("%a %d/%m/%Y")
@@ -41,10 +46,17 @@ hide_st_style = """
     </stile>
 """
 st_title = f"""
-<div style="position: absolute; top: 150px; left: -200px; height: 100vh; font-size: 35px; font-weight: bold; color: {color}">
+<div style="position: absolute; top: 10px; left: -300px; height: 100px; font-size: 35px; font-weight: bold; color: {st.session_state.color}">
     {date_to_print}
 </div>
 """
+print_m = f"""
+    <div style="display: grid; place-items: center; height: 100%;">
+        <div style="font-size: 100px; font-weight: bold; color: {st.session_state.color};">
+            {time_to_print}
+        </div>
+    </div>
+    """
 st.markdown(st_title, unsafe_allow_html=True)
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
@@ -56,7 +68,7 @@ background-size: 100% auto;
 background-position: center center;
 background-repeat: no-repeat;
 background-attachment: local;
-margin-top:-140px;
+
 }}
 [data-testid="stHeader"] {{
 background: rgba(0,0,0,0);
@@ -66,12 +78,16 @@ background: rgba(0,0,0,0);
 
 
 
-print_m = f"""<div style="display: flex; justify-content: center; align-items: center; height: 100vh; font-size: 100px; font-weight: bold; margin-top: -100px; color: {color}">
-    {time_to_print}
-</div>
-"""
+# print_m = f"""
+#     <div style="display: flex; justify-content: center; align-items: center; width: {st.session_state.screen_dim[0]}px; height: {st.session_state.screen_dim[1]}px; font-size: 100px; font-weight: bold; color: {st.session_state.color}">
+#         {time_to_print}
+#     </div>
+#     """
 
 st.markdown(page_bg_img, unsafe_allow_html=True)
 st.markdown(print_m, unsafe_allow_html=True)
+
+
+
 time.sleep(1)
 st.rerun()
--- pages/2_⚙️_settings.py
+++ pages/2_⚙️_settings.py
@@ -9,24 +9,27 @@ st.set_page_config(
 )
 st.title("Settings:")
 
-hide_st_style = """
-    <style>
-    header {visibility: hidden;}
-    </stile>
-"""
+hide_st_style = """<style> header {visibility: hidden;} footer {visibility: hidden;} </stile>"""
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
+if "timezone" not in st.session_state:
+    st.session_state.timezone = "Europe/Rome"  
+
+if "color" not in st.session_state:
+    st.session_state.color = "#ffffff"
+    
+if "screen_dim" not in st.session_state:
+    st.session_state.screen_dim = [1920, 1080]
 
 if "timezones" not in st.session_state:
-  
+    #timezones to execute in the following format Area/Main_City
     timezones = available_timezones()
-    
-    filtered_time_zones = [tz for tz in timezones if "/" in tz and "Etc" not in tz]
-    
+    filtered_time_zones = [tz for tz in timezones if "/" in tz and "Etc" not in tz] #removal of undesired timez
     filtered_time_zones.sort()
     st.session_state.timezones = filtered_time_zones
-print(st.session_state.timezones)
+
 if "timezones_tp" not in st.session_state:
+  #timezones to print in the following format Area: Main City
   timezones_tp = []
   for elem in st.session_state.timezones:
     elem = elem.replace("/", ": ", 1)
@@ -34,10 +37,20 @@ if "timezones_tp" not in st.session_state:
     elem =elem.replace("_", " ")
     timezones_tp.append(elem)
   st.session_state.timezones_tp = timezones_tp
-print(st.session_state.timezones_tp)
+
+#function to change format from printable to executable
 def format_funct(val):
   i = st.session_state.timezones.index(val)
   return st.session_state.timezones_tp[i]
 
-st.session_state.timezone = st.selectbox("Select Timezone:", st.session_state.timezones, index=424, format_func=format_funct, placeholder="Europe: Italy")
+#function to extract the index of the current timezone:
+def get_index(val):
+  return st.session_state.timezones.index(val)
 
+#save timezone value:
+st.session_state.timezone = st.selectbox("Select Timezone:", st.session_state.timezones, index=get_index(st.session_state.timezone), format_func=format_funct, placeholder="Europe: Italy")
+
+st.session_state.color = st.color_picker("select a color", st.session_state.color)
+
+st.session_state.screen_dim[0] = st.number_input("select width pixels", min_value=1, value = st.session_state.screen_dim[0])
+st.session_state.screen_dim[1] = st.number_input("select height pixels", min_value=1, value = st.session_state.screen_dim[1])
