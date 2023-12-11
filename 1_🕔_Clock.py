438c2699ee8870f78e0b62c3c405a051a7027c7a
Samuele Vianello <samuele.vianello2002@gmail.com>
2023-12-11 17:41:13 +0100

    improvements on design

--- 1_🕔_Clock.py
+++ 1_🕔_Clock.py
@@ -13,10 +13,10 @@ st.set_page_config(
 
 
 #link_gif = "https://www.icegif.com/wp-content/uploads/snow-icegif-29.gif" 
-link_gif = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a2205e2c-52eb-46b3-89fb-bd0fd5da780e/dbyn08r-83e1c070-bfa6-48da-ae25-b7d7409639fe.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2EyMjA1ZTJjLTUyZWItNDZiMy04OWZiLWJkMGZkNWRhNzgwZVwvZGJ5bjA4ci04M2UxYzA3MC1iZmE2LTQ4ZGEtYWUyNS1iN2Q3NDA5NjM5ZmUuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.pFhMtU06SUSgNo6CA5fLFNJlv9h69s9oE89E2E6iAGk"
+#link_gif = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a2205e2c-52eb-46b3-89fb-bd0fd5da780e/dbyn08r-83e1c070-bfa6-48da-ae25-b7d7409639fe.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2EyMjA1ZTJjLTUyZWItNDZiMy04OWZiLWJkMGZkNWRhNzgwZVwvZGJ5bjA4ci04M2UxYzA3MC1iZmE2LTQ4ZGEtYWUyNS1iN2Q3NDA5NjM5ZmUuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.pFhMtU06SUSgNo6CA5fLFNJlv9h69s9oE89E2E6iAGk"
 
 
-#link_gif = "https://f8n-production.s3.us-east-2.amazonaws.com/collections/awzn1l17u-beach%20relax-export.gif"
+link_gif = "https://f8n-production.s3.us-east-2.amazonaws.com/collections/awzn1l17u-beach%20relax-export.gif"
 
 
 
@@ -45,14 +45,20 @@ hide_st_style = """
     header {visibility: hidden;}
     </stile>
 """
+date_pix = int(3.25*st.session_state.screen_dim[1]/100)
+date_left = int(st.session_state.screen_dim[0]*0.16)
+date_up =  int(st.session_state.screen_dim[1]*0.02)
+
+time_pix = int(9.25*st.session_state.screen_dim[1]/100)
+
 st_title = f"""
-<div style="position: absolute; top: 10px; left: -300px; height: 100px; font-size: 35px; font-weight: bold; color: {st.session_state.color}">
+<div style="position: absolute; top: {date_up}px; left: -{date_left}px; height: 100px; font-size: {date_pix}px; font-weight: bold; color: {st.session_state.color}">
     {date_to_print}
 </div>
 """
 print_m = f"""
-    <div style="display: grid; place-items: center; height: 100%;">
-        <div style="font-size: 100px; font-weight: bold; color: {st.session_state.color};">
+    <div style="display: grid; place-items: center; height: center; padding-top: 7%;">
+        <div style="font-size: {time_pix}px; font-weight: bold; color: {st.session_state.color};">
             {time_to_print}
         </div>
     </div>
